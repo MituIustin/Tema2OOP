0e02db7f1321544b38b06594ba1ad2c67f30f61c
IustinMitu <iustin-aurelian.mitu@s.unibuc.ro>
2023-04-29 20:44:26 +0200

    Tema 2

--- Clase/Exception.cpp
+++ Clase/Exception.cpp
@@ -1,6 +1,9 @@
 #include "Exception.h"
+#include <iostream>
 
-Exception::Exception(const std::string& message) : message_(message) {}
+Exception::Exception(const std::string& message) : message_(message) {
+    std::cout<<"Constructor Exceptie\n";
+}
 
 const char* Exception::what() const noexcept {
     return message_.c_str();
