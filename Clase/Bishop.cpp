bab27fe408bf40c89534e708a0872a5d105c4c43
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:51:22 +0300

    Update Bishop.cpp

--- Clase/Bishop.cpp
+++ Clase/Bishop.cpp
@@ -1,25 +1,26 @@
 #include "Bishop.h"
+
 #include <iostream>
 #include <cmath>
 #include <memory>
 
 Bishop::Bishop() {
-    std::cout<<"Constructor Bishop\n";
-    alive = true;
-    name = "bishop";
+   std::cout << "Constructor Bishop\n";
+   alive = true;
+   name = "bishop";
 }
 
-std::shared_ptr<Piece> Bishop::clone() const {
-    std::cout<<"Constructor Clone Bishop\n";
-    return std::make_shared<Bishop>(Bishop(*this));
+std::shared_ptr < Piece > Bishop::clone() const {
+   std::cout << "Constructor Clone Bishop\n";
+   return std::make_shared < Bishop > (Bishop( * this));
 }
 
 bool Bishop::move(int x1, int y1, int x2, int y2, bool ok) {
-    if (x1==x2 && y1==y2) return false;
-    if(abs(x1-x2) == abs(y1-y2) || abs(x1-y1) == abs(x2-y2)) return true;
-    return false;
+   if (x1 == x2 && y1 == y2) return false;
+   if (abs(x1 - x2) == abs(y1 - y2) || abs(x1 - y1) == abs(x2 - y2)) return true;
+   return false;
 }
 
 Bishop::~Bishop() {
-    std::cout<<"Destructor Bishop\n";
+   std::cout << "Destructor Bishop\n";
 }
