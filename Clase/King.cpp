e5aaae8f4e52283b8c2b2279be5d23e4e97042af
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:57:02 +0300

    Update King.cpp

--- Clase/King.cpp
+++ Clase/King.cpp
@@ -1,24 +1,25 @@
 #include "King.h"
+
 #include <iostream>
 #include <cmath>
+
 King::King() {
-    std::cout<<"Constructor King\n";
+    std::cout << "Constructor King\n";
     alive = true;
     name = "king";
 }
 
-std::shared_ptr<Piece> King::clone() const {
-    std::cout<<"Constructor Clone King\n";
-    return std::make_shared<King>(King(*this));
+std::shared_ptr < Piece > King::clone() const {
+    std::cout << "Constructor Clone King\n";
+    return std::make_shared < King > (King( * this));
 }
 
 bool King::move(int x1, int y1, int x2, int y2, bool ok) {
-    if (x1==x2 && y1==y2) return false;
-    if(abs(x1-x2)<=1 && abs(y1-y2)<=1) return true;
+    if (x1 == x2 && y1 == y2) return false;
+    if (abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1) return true;
     return false;
 }
 
-
 King::~King() {
-    std::cout<<"Destructor King\n";
+    std::cout << "Destructor King\n";
 }
