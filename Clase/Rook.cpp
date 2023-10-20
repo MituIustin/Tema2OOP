4ff88e21be81a006abd213f97109aca21038a94c
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 12:04:51 +0300

    Update Rook.cpp

--- Clase/Rook.cpp
+++ Clase/Rook.cpp
@@ -1,26 +1,27 @@
 #include "Rook.h"
+
 #include <iostream>
 #include <memory>
 
 Rook::Rook() {
-    std::cout<<"Constructor Rook\n";
+    std::cout << "Constructor Rook\n";
     alive = true;
     name = "rook";
 }
 
-std::shared_ptr<Piece> Rook::clone() const {
+std::shared_ptr < Piece > Rook::clone() const {
     std::cout << "Constructor Clone Rook\n";
-    return std::make_shared<Rook>(*this);
+    return std::make_shared < Rook > ( * this);
 }
 
-
 bool Rook::move(int x1, int y1, int x2, int y2, bool ok) {
-    if (x1==x2 && y1==y2) return false;
-    bool var = true;
-    if(x1 != x2 && y1 != y2) var = false;
+    if (x1 == x2 && y1 == y2) return false;
+    bool
+    var = true;
+    if (x1 != x2 && y1 != y2) var = false;
     return var;
 }
 
 Rook::~Rook() {
-    std::cout<<"Destructor Rook\n";
+    std::cout << "Destructor Rook\n";
 }
