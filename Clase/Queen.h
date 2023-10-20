ac202613e1fd66c9c8c64917ac465dec0e37f472
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 12:04:29 +0300

    Update Queen.h

--- Clase/Queen.h
+++ Clase/Queen.h
@@ -2,16 +2,15 @@
 #define OOP_QUEEN_H
 
 #include "Piece.h"
+
 #include <string>
 #include <memory>
 
-class Queen : public  Piece{
-public:
-    Queen();
-    std::shared_ptr<Piece> clone() const override;
-    bool move(int,int,int,int,bool) override;
+class Queen: public Piece {
+    public: Queen();
+    std::shared_ptr < Piece > clone() const override;
+    bool move(int, int, int, int, bool) override;
     ~Queen();
 };
-
 
 #endif //OOP_QUEEN_H
