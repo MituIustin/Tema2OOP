cc7ca165bd440acd0fba09cb9b0145fc8e04815e
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:59:44 +0300

    Update Pawn.cpp

--- Clase/Pawn.cpp
+++ Clase/Pawn.cpp
@@ -1,25 +1,26 @@
 #include "Pawn.h"
+
 #include <iostream>
 #include <cmath>
 #include <memory>
 
-Pawn::Pawn(){
-    std::cout<<"Constructor Pawn\n";
+Pawn::Pawn() {
+    std::cout << "Constructor Pawn\n";
     alive = true;
     name = "pawn";
 }
 
-std::shared_ptr<Piece> Pawn::clone() const {
-    std::cout<<"Constructor Clone Pawn\n";
-    return std::make_shared<Pawn>( Pawn(*this) );
+std::shared_ptr < Piece > Pawn::clone() const {
+    std::cout << "Constructor Clone Pawn\n";
+    return std::make_shared < Pawn > (Pawn( * this));
 }
 
 bool Pawn::move(int x1, int x2, int y1, int y2) {
-    if (x1==y1 && x2==y2) return false;
-    if(x2==y2 && abs(x1-y1)<=2) return true;
+    if (x1 == y1 && x2 == y2) return false;
+    if (x2 == y2 && abs(x1 - y1) <= 2) return true;
     return false;
 }
 
 Pawn::~Pawn() {
-    std::cout<<"Destructor Pawn\n";
+    std::cout << "Destructor Pawn\n";
 }
