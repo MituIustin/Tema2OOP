a91de5379f03270ac9fa56889bffd0a06392e2fa
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:57:32 +0300

    Update King.h

--- Clase/King.h
+++ Clase/King.h
@@ -2,15 +2,15 @@
 #define OOP_KING_H
 
 #include "Piece.h"
+
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <memory>
 
-class King : public Piece{
-public:
-    King();
-    std::shared_ptr<Piece> clone() const override;
-    bool move(int,int,int,int,bool) override;
+class King: public Piece {
+    public: King();
+    std::shared_ptr < Piece > clone() const override;
+    bool move(int, int, int, int, bool) override;
     ~King();
 };
 
