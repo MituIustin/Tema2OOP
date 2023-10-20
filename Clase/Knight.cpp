977394c2844520c32b577020b7cd7eb691f58c40
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:58:31 +0300

    Update Knight.cpp

--- Clase/Knight.cpp
+++ Clase/Knight.cpp
@@ -1,31 +1,32 @@
 #include "Knight.h"
+
 #include <iostream>
 #include <memory>
 
-Knight::Knight(){
-    std::cout<<"Constructor Knight\n";
+Knight::Knight() {
+    std::cout << "Constructor Knight\n";
     alive = true;
     name = "knight";
 }
 
-std::shared_ptr<Piece> Knight::clone() const {
-    std::cout<<"Constructor Clone Knight\n";
-    return std::make_shared<Knight>(Knight(*this));
+std::shared_ptr < Piece > Knight::clone() const {
+    std::cout << "Constructor Clone Knight\n";
+    return std::make_shared < Knight > (Knight( * this));
 }
 
-bool Knight::move(int x1, int y1, int x2, int y2,bool ok) {
-    if (x1==x2 && y1==y2) return false;
-    if(x1 == x2 - 2 && y1 == y2 - 1) return true;
-    if(x1 == x2 - 2 && y1 == y2 + 1) return true;
-    if(x1 == x2 + 2 && y1 == y2 - 1) return true;
-    if(x1 == x2 + 2 && y1 == y2 + 1) return true;
-    if(x1 == x2 - 1 && y1 == y2 - 2) return true;
-    if(x1 == x2 - 1 && y1 == y2 + 2) return true;
-    if(x1 == x2 + 1 && y1 == y2 - 2) return true;
-    if(x1 == x2 + 1 && y1 == y2 + 2) return true;
+bool Knight::move(int x1, int y1, int x2, int y2, bool ok) {
+    if (x1 == x2 && y1 == y2) return false;
+    if (x1 == x2 - 2 && y1 == y2 - 1) return true;
+    if (x1 == x2 - 2 && y1 == y2 + 1) return true;
+    if (x1 == x2 + 2 && y1 == y2 - 1) return true;
+    if (x1 == x2 + 2 && y1 == y2 + 1) return true;
+    if (x1 == x2 - 1 && y1 == y2 - 2) return true;
+    if (x1 == x2 - 1 && y1 == y2 + 2) return true;
+    if (x1 == x2 + 1 && y1 == y2 - 2) return true;
+    if (x1 == x2 + 1 && y1 == y2 + 2) return true;
     return false;
 }
 
 Knight::~Knight() {
-    std::cout<<"Destructor Knight\n";
+    std::cout << "Destructor Knight\n";
 }
