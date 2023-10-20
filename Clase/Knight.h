9966c8e5c3cf85b264af1487d467cdc0da9111e8
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 11:58:59 +0300

    Update Knight.h

--- Clase/Knight.h
+++ Clase/Knight.h
@@ -2,16 +2,15 @@
 #define OOP_KNIGHT_H
 
 #include "Piece.h"
+
 #include <string>
 #include <memory>
 
-class Knight : public Piece{
-public:
-    Knight();
-    std::shared_ptr<Piece> clone() const override;
-    bool move(int,int,int,int,bool) override;
+class Knight: public Piece {
+    public: Knight();
+    std::shared_ptr < Piece > clone() const override;
+    bool move(int, int, int, int, bool) override;
     ~Knight();
 };
-
 
 #endif //OOP_KNIGHT_H
