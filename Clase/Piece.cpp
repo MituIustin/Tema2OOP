74c2701fbf20f3b6db9df0b8ac92282098d6881f
MituIustin <126506678+MituIustin@users.noreply.github.com>
2023-10-20 12:00:53 +0300

    Update Piece.cpp

--- Clase/Piece.cpp
+++ Clase/Piece.cpp
@@ -1,49 +1,48 @@
 #include "Piece.h"
 #include "Exception.h"
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 int Piece::number_of_pieces = 0;
 
 Piece::Piece() {
-    std::cout<<"Constructor Piece\n";
+    std::cout << "Constructor Piece\n";
     alive = true;
     row = 9;
     col = 9;
     number_of_pieces++;
-    std::cout<<number_of_pieces<<" pieces have been created since the game started.\n";
+    std::cout << number_of_pieces << " pieces have been created since the game started.\n";
 }
 
 Piece::Piece(const Piece & other):
-row(other.row),
-col(other.col),
-alive(other.alive),
-name(other.name),
-texture(other.texture),
-sprite(other.sprite)
-{
-    std::cout<<"CC Piece. \n";
-}
+    row(other.row),
+    col(other.col),
+    alive(other.alive),
+    name(other.name),
+    texture(other.texture),
+    sprite(other.sprite) {
+        std::cout << "CC Piece. \n";
+    }
 
-Piece &Piece::operator=(const Piece & other) {
+Piece & Piece::operator = (const Piece & other) {
     name = other.name;
     alive = other.alive;
     texture = other.texture;
     sprite = other.sprite;
     row = other.row;
     col = other.col;
-    return *this;
+    return * this;
 }
 
-
-bool Piece::move(int x1,int x2,int x3,int x4, bool ok) {
-    if(x1 == 0 && x2 == 0 && x3 == 0 && x4 == 0 && ok == true) return true;
+bool Piece::move(int x1, int x2, int x3, int x4, bool ok) {
+    if (x1 == 0 && x2 == 0 && x3 == 0 && x4 == 0 && ok == true) return true;
     return false;
 }
 
 void Piece::set_sprite(sf::Sprite s) {
     sprite = s;
-    sprite.setPosition(row*100,col*100);
+    sprite.setPosition(row * 100, col * 100);
 }
 
 bool Piece::get_alive() {
@@ -69,7 +68,7 @@ sf::Sprite Piece::get_sprite() {
 void Piece::set_row_col(int x, int y) {
     col = x;
     row = y;
-    if(x >=0 && x<=9 && y>=0 && y<=9) sprite.setPosition(row*100, col*100);
+    if (x >= 0 && x <= 9 && y >= 0 && y <= 9) sprite.setPosition(row * 100, col * 100);
     else throw NotValidXY();
 }
 
@@ -81,12 +80,10 @@ void Piece::set_color(std::string clr) {
     color = clr;
 }
 
-
 Piece::~Piece() {
-    std::cout<<"Destructor Piece\n";
+    std::cout << "Destructor Piece\n";
     number_of_pieces--;
 }
-
 
 std::string Piece::get_name() {
     return name;
